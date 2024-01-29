59e677004e8f8c4c70ed5e2f94136c2392d9406f
EmadAA <emadadil999@gmail.com>
2024-01-29 23:55:46 +0600

    Contest Code

--- adil.java
+++ adil.java
@@ -1,8 +1,20 @@
 // Name: Emad Uddin Adil
+
+import java.util.Scanner;
+
 public class adil {
 
     public static void main(String[] args) {
-        System.out.println("Hello Adil j");
+        
+        Scanner scan = new Scanner(System.in);
+        char section = scan.next().charAt(0); 
+        if (section == 'A' || section == 'B' || section == 'E' || section == 'a' || section == 'b' || section == 'e') {
+            System.out.println("JIM Sir");
+        } else if (section == 'C' || section == 'D' || section == 'c' || section == 'd') {
+            System.out.println("JAC Sir");
+        } else {
+            System.out.println("Invalid input");
+        }
         
     }
 }
