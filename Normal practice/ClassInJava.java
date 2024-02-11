aa46448d6676bbb1bab2ce79a689e4dbe64c5c3d
EmadAA <emadadil999@gmail.com>
2024-02-11 18:57:57 +0600

    Normal Commit 2

--- Normal practice/ClassInJava.java
+++ Normal practice/ClassInJava.java
@@ -4,6 +4,10 @@ public class ClassInJava {
     public static void main(String[] args) {
         
    System.out.println("Hello Adil ");
+   System.out.println("Hello Adil 2 ");
+   System.out.println("Hello Adil 3 ");
+   System.out.println("Hello Adil "+ 4 );
+   System.out.println("Hello Adil  5");
     }
 }
 
