4a328346609c686a9362c7dae5b30f119213997c
EmadAA <emadadil999@gmail.com>
2024-05-08 22:06:47 +0600

    java

---
+++ Normal practice/Adil.java
@@ -0,0 +1,7 @@
+package Normal practice;
+
+public class Adil {
+    public static void main(String[] args) {
+        System.out.println("Hello Adil");
+    }
+}
---
+++ Normal practice/Test.java
@@ -0,0 +1,15 @@
+/**
+ * Test
+ */
+import java.util.Scanner;
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello Adil");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
+        System.out.println("Hello Adil");
+
+
+    }
+}
